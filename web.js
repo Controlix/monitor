12e377ea18a58c45a0bc44f193de0bc9174e9327
Marc Bogaerts <marcbogaerts@telenet.be>
2016-06-04 17:19:19 +0200

    Read hosts from file

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var request = require('request');
+var hosts = require('./hosts.json');
+
 var app = express();
 
 var info = {};
@@ -14,37 +16,33 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-function loop(host, port, url) {
-	info.hosts[host] = {};
-	console.log('http://' + host + ':' + port + url);
+function loop(host) {
+	info.hosts[host.host] = {};
 
 	var req = request.defaults({
-      baseUrl: 'http://' + host + ':' + port + url
+      baseUrl: 'http://' + host.host + ':' + host.port + host.url
 	});
 
 	req
 	  .get('/health')
 	  .on('data', function (data) {
-	  	info.hosts[host].status = JSON.parse(data).status;
+	  	info.hosts[host.host].status = JSON.parse(data).status;
 	  })
 	  .on('error', function(error) {
-	  	info.hosts[host].status = 'UNKNOWN';
+	  	info.hosts[host.host].status = 'UNKNOWN';
 	  });
 
 	req
 	  .get('/info')
 	  .on('data', function (data) {
-	  	info.hosts[host].app = JSON.parse(data).app;
+	  	info.hosts[host.host].app = JSON.parse(data).app;
 	  })
 	  .on('error', function(error) {
-	  	info.hosts[host].app = 'UNKNOWN';
+	  	info.hosts[host.host].app = 'UNKNOWN';
 	  });
 
 	info.count++;
-	setTimeout(function () {loop(host, port, url);}, 1000);
+	setTimeout(function () {loop(host);}, 1000);
 }
 
-
-
-loop('localhost', 8090, '/');
-loop('nieuw.ad.nl', 80, '/monitor');
+hosts.forEach(function (host) { loop(host); });
